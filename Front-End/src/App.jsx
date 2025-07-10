bdbad2e538eefae9d0c05d9cc3e309515d555265
Parth Singh <parths.singh2003@gmail.com>
2025-07-11 01:32:16 +0530

    Added a Clean Vite and React front end for Cleanify

---
+++ Front-End/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>Cleanify 🌍</h1>
+      <p>Welcome to Cleanify-Report and track waste in your area.</p>
+    </div>
+  );
+}
+
+export default App;
